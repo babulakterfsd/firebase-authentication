426527c2a140dba7d2c956be09046010e0ee361f
babulakterfsd <babulakterfsd@gmail.com>
2021-10-07 22:45:39 +0600

    signup,signin,email verification & pwd reset added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ import {
   GoogleAuthProvider,
   GithubAuthProvider,
   signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
 } from "firebase/auth";
 import initializeAuthentication from "./Firebase/firebase.initialize";
-import { Container, Row, Button, Col, Card } from "react-bootstrap";
+import { Container, Row, Button, Col, Card, Form } from "react-bootstrap";
 import { useState } from "react";
 
 initializeAuthentication();
@@ -18,6 +22,12 @@ const auth = getAuth();
 
 function App() {
   const [user, setUser] = useState({});
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [error, setError] = useState("");
+  let [passwordRulesShow, setPasswordRulesShow] = useState(false);
+  let [response, setResponse] = useState("");
+  let [isRegistered, setIsRegistered] = useState(false);
 
   //google signin button
   const handleGoogleSignIn = () => {
@@ -46,22 +56,178 @@ function App() {
     });
   };
 
+  //handle Submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    !isRegistered
+      ? register(userEmail, userPassword)
+      : login(userEmail, userPassword);
+  };
+
+  //login existing user
+  const login = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        setResponse("Login Successful");
+      })
+      .catch((error) => {
+        setResponse(error.message);
+      });
+  };
+
+  //create new user
+  const register = (email, password) => {
+    if (userPassword.length < 8) {
+      setError("password must be at least 8 characters");
+      return;
+    } else {
+      setError("");
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((result) => {
+          const user = result.user;
+          setResponse("Registration Successful");
+          verifyEmail();
+        })
+        .catch((error) => {
+          setResponse(error.message);
+        });
+    }
+  };
+  //verify email
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser);
+  };
+
+  //handle Password
+  const handlePassword = (event) => {
+    setUserPassword(event.target.value);
+  };
+  //reset forgot password
+  const resetPassword = () => {
+    if (userEmail) {
+      sendPasswordResetEmail(auth, userEmail);
+      setResponse("Password reset email sent !");
+    } else {
+      setResponse("Email field is empty");
+    }
+  };
+
+  //handle Email
+  const handleEmail = (event) => {
+    setUserEmail(event.target.value);
+  };
+
+  // passwordRulesDisplayer
+  const passwordRulesDisplayer = () => {
+    passwordRulesShow = !passwordRulesShow;
+    setPasswordRulesShow(passwordRulesShow);
+  };
+  //ToggleForm
+  const toggleForm = (event) => {
+    setIsRegistered(event.target.checked);
+  };
+
   return (
     <div className="App">
+      {/* Form */}
+      <Container>
+        <Row>
+          <div className="col-12 col-md-8 col-lg-6 mx-auto">
+            <Form className="mt-5 shadow-sm p-5" onSubmit={handleSubmit}>
+              <h5 className="text-secondary mb-3">
+                {" "}
+                <i className="fas fa-user-tie text-success"></i> Please
+                {!isRegistered ? " Resgister !" : " LogIn !"}
+              </h5>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  onBlur={handleEmail}
+                  type="email"
+                  placeholder="Enter email"
+                  required
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  onBlur={handlePassword}
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
+                <Form.Text className="text-danger">{error}</Form.Text>
+                <Form.Text className="text-danger">
+                  {passwordRulesShow ? (
+                    <ul className="text-muted">
+                      <li>Password must contain 2 uppercase letters</li>
+                      <li>must incluse at least one special character</li>
+                      <li>must contain at least three lowercase letters</li>
+                      <li>must incluse at least two digits</li>
+                      <li>Password should be at least 8 characters long</li>
+                    </ul>
+                  ) : (
+                    <span></span>
+                  )}
+                </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <div className="d-flex justify-content-between">
+                  <Form.Check
+                    onChange={toggleForm}
+                    type="checkbox"
+                    label="Already Registered?"
+                  />
+                  {!isRegistered ? (
+                    <Button
+                      onClick={passwordRulesDisplayer}
+                      className="btn-sm btn-success text-white fw-semi-bold"
+                    >
+                      password rules
+                    </Button>
+                  ) : (
+                    <Button
+                      onClick={resetPassword}
+                      className="btn-sm btn-danger text-white fw-semi-bold"
+                    >
+                      Forgot password?
+                    </Button>
+                  )}
+                </div>
+              </Form.Group>
+              <p className="text-secondary">{response}</p>
+              <Button variant="success" type="submit" className="px-4 py-2">
+                <i className="fas fa-sign-in-alt me-2 text-warning"></i>
+                {!isRegistered ? " Resgister !" : " LogIn !"}
+              </Button>
+            </Form>
+          </div>
+        </Row>
+      </Container>
+
+      <div className="divider">
+        <hr className="w-50 mx-auto" />
+        <h6 className="text-center text-secondary">or</h6>
+      </div>
       <Container>
         <Row>
           {!user.name ? (
             <div className="col-12 col-md-10 col-lg-8 mx-auto">
               <div className="button-container d-flex justify-content-center">
                 <Button
-                  className="btn-success fw-semi-bold my-5 me-5"
+                  className="btn-success fw-semi-bold  me-1"
                   onClick={handleGoogleSignIn}
                 >
                   <i className="fab fa-google me-2 text-warning"></i>
                   SignIn with Google
                 </Button>
                 <Button
-                  className="btn-success fw-semi-bold my-5"
+                  className="btn-success fw-semi-bold"
                   onClick={handleGithubSignIn}
                 >
                   <i className="fab fa-github me-2 text-warning"></i>
